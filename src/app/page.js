91cc80b448f315f7dd42fc61020efe1def7e4b9d
adielrum <adiel.rum@sman2bdg.sch.id>
2024-07-07 20:49:38 +0700

    Removed login feature for practical

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,6 @@ import { getCurrentUser } from "@/libs/session";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const user = await getCurrentUser();
-
-  if (!user) {
-    redirect("/sign-in");
-  }
 
   return (
     <section className="">
